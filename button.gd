ec77ed9091a7f5b0a92a3c79ad4b5b656026404f
N-O-M-I-S <macleannio2010@gmail.com>
2025-02-09 13:57:15 +1300

    HEL[[p

--- button.gd
+++
@@ -1,3 +0,0 @@
-extends Node
-
-var button_active = false
--- scenes/environment/button_door.gd
+++
@@ -1,8 +0,0 @@
-extends  Node2D
-@export var collision: CollisionObject2D
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if GlobalButton.button_active == true:
-		pass

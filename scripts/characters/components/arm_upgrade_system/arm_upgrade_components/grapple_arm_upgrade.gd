33ffe21219d8e3ad4fcc1e487e783a3af080d970
cookiemonsternz <Christopher.body@mcglashan.school.nz>
2025-02-25 19:46:48 +1300

    restructured assets folder (sorry nio) + more mountains

--- scripts/characters/components/arm_upgrade_system/arm_upgrade_components/grapple_arm_upgrade.gd
+++ scripts/characters/components/arm_upgrade_system/arm_upgrade_components/grapple_arm_upgrade.gd
@@ -1,6 +1,6 @@
 class_name GrappleArmUpgrade extends ArmUpgrade
 
-var loaded_texture: Texture = preload("res://assets/Images/grapple_arm_upgrade.png")
+var loaded_texture: Texture = preload("res://assets/images/ui/arm_upgrade_menu/arm_upgrades/grapple_arm_upgrade.png")
 
 const launch_speed = 15
 const damping = 0.9

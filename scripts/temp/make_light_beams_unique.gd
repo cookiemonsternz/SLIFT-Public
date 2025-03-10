6838705e5567db22a894b346a4f87c3ff7588a33
cookiemonsternz <Christopher.body@mcglashan.school.nz>
2025-03-10 22:57:06 +1300

    fix weird shadows

--- scripts/temp/make_light_beams_unique.gd
+++ scripts/temp/make_light_beams_unique.gd
@@ -1,6 +1,8 @@
 @tool
 extends Node2D
 
+@export var shade: Shader
+
 @export var mybutton: bool:
 	set(value):
 		on_do_stuff()
@@ -10,8 +12,11 @@ func on_do_stuff():
 	var arr = []
 	for child: ColorRect in get_children():
 		print(child)
+		child.material.set("shader", shade)
+		child.material.set("shader_parameter/color", Vector4(1.0, 0.95, 0.825, 0.25))
 		child.material = child.material.duplicate(true)
 		child.material.set_shader_parameter("shader_parameter/seed", randf_range(-50, 50))
+		
 		print(child.material.get_shader_parameter("shader_parameter/seed"))
 	for child in get_children():
 		arr.append(child.material.get_shader_parameter("shader_parameter/seed"))

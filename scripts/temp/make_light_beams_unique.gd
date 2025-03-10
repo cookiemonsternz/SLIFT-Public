d902dbac440b422dea09b8a07adc1a0da991a5e5
cookiemonsternz <Christopher.body@mcglashan.school.nz>
2025-03-10 19:03:01 +1300

    lightbeams placed

---
+++ scripts/temp/make_light_beams_unique.gd
@@ -0,0 +1,25 @@
+@tool
+extends Node2D
+
+@export var mybutton: bool:
+	set(value):
+		on_do_stuff()
+
+func on_do_stuff():
+	print(get_children())
+	var arr = []
+	for child: ColorRect in get_children():
+		print(child)
+		child.material = child.material.duplicate(true)
+		child.material.set_shader_parameter("shader_parameter/seed", randf_range(-50, 50))
+		print(child.material.get_shader_parameter("shader_parameter/seed"))
+	for child in get_children():
+		arr.append(child.material.get_shader_parameter("shader_parameter/seed"))
+	# print amount of each seed
+	var seed_counts = {}
+	for seed_value in arr:
+		if seed_value in seed_counts:
+			seed_counts[seed_value] += 1
+		else:
+			seed_counts[seed_value] = 1
+	print("Seed counts: ", seed_counts)

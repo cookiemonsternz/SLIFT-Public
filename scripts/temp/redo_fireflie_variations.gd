ef5339a6c14f0d3fd6df9baa0bc03feb934cb42d
cookiemonsternz <Christopher.body@mcglashan.school.nz>
2025-03-26 20:21:21 +1300

    change firefly color

--- scripts/temp/redo_fireflie_variations.gd
+++ scripts/temp/redo_fireflie_variations.gd
@@ -1,11 +1,13 @@
+@tool
 extends Node2D
 
+@export var do_thing: bool: 
+	set(value):
+		do_func()
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func do_func():
+	for child in get_children():
+		if child is GPUParticles2D:
+			child.process_material.set_param_min(ParticleProcessMaterial.PARAM_HUE_VARIATION, 0.0)
+			child.process_material.set_param_max(ParticleProcessMaterial.PARAM_HUE_VARIATION, 0.2)
